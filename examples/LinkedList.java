766d9a9ec85e2b20fc637321ef7432ffb4335023
Clemens Lang <neverpanic@gmail.com>
2014-05-28 00:07:14 +0200

    Rework LinkedList example to be shorter

--- examples/LinkedList.java
+++ examples/LinkedList.java
@@ -10,13 +10,13 @@ public class LinkedList<T> {
 
 	private ListElement<T> head;
 
-	public void addElement(T element) {
-		insert(this, new ListElement<>(element));
+	public void addElement(T elem) {
+		insert(this, new ListElement<>(elem));
 	}
 
 	private static <V> void insert(LinkedList<V> list,
-	                               ListElement<V> newElement) {
-		newElement.next = list.head;
-		list.head = newElement;
+	                               ListElement<V> elem) {
+		elem.next = list.head;
+		list.head = elem;
 	}
 }
